af5efa6d56b1a41bb808e819f80071476821b087
s8186255 <andrew-su@qq.com>
2024-10-01 22:59:49 +0800

    initialize project

---
+++ src/Frontend/Frontend/Program.cs
@@ -0,0 +1,39 @@
+using Frontend.Client.Pages;
+using Frontend.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5010); // HTTP
+    options.ListenAnyIP(7136, listenOptions => listenOptions.UseHttps()); // HTTPS
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Frontend.Client._Imports).Assembly);
+
+app.Run();
